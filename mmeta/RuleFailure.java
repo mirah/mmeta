5be8063aded541ce5aefea2604d0f65341a1271e
vkozyr <kozyr@programpark.ru>
2016-03-24 17:31:41 +0300

    make used exceptions to extend runtime exception as it actually not checked or declared thrown

--- mmeta/RuleFailure.java
+++ mmeta/RuleFailure.java
@@ -1,7 +1,7 @@
 // copyright 2009 ActiveVideo; license: MIT; see license.txt
 package mmeta;
 
-public class RuleFailure extends Exception {
+public class RuleFailure extends RuntimeException {
     public String last = "";
     public String toString() { return "ERROR.last: "+ last; }
 }
--- mmeta/ast.mirah
+++ mmeta/ast.mirah
@@ -6,7 +6,7 @@ import java.util.ArrayList
 import java.util.Collections
 import java.util.List
 
-class Grow < Exception
+class Grow < RuntimeException
 end
 
 class Position; implements Serializable
